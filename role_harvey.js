93d55ee9a2a19c77dc4f08e113f796f86b621d80
Evan Johnson <johnsonevanpatrick@gmail.com>
2024-12-11 09:45:33 -0700

    harvesters need to return energy to extensions in order to build stronger creeps

--- role_harvey.js
+++ role_harvey.js
@@ -6,15 +6,17 @@ module.exports = {
     {
         var return_loc1 = spawn;
         //console.log(spawn);
-        /*var return_loc2 = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        var return_loc2 = creep.pos.findClosestByPath(FIND_STRUCTURES, {
             filter: (s) => (s.structureType == STRUCTURE_EXTENSION) && s.energy < s.energyCapacity
-        });*/
+        });
         var return_loc3 = creep.room.controller;
         
         if (creep.memory.twerking)
         {
             if (return_loc1.energy < 300)
                 returnGold.run(creep, return_loc1);
+            else if (return_loc2 != null) 
+                returnGold.run(creep, return_loc2);
             else
                 returnGold.run(creep, return_loc3);
         } else
