e5e4a1ccf038175d0ed547dc3b3410c49157a82a
Evan Johnson <johnsonevanpatrick@gmail.com>
2024-12-10 17:32:41 -0700

    Attempting to reduce CPU cost for builders

--- role_bob.js
+++ role_bob.js
@@ -5,15 +5,16 @@ var returnGold = require('return_gold');
 module.exports = {
     run: function (creep)
     {
-        var return_loc1 = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+        
         /*var return_loc2 = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                 filter: (s) => (s.structureType == STRUCTURE_TOWER) && s.energy < s.energyCapacity
         }, "astar");
         */
-        var return_loc3 = creep.room.controller;
         //console.log("hey");
         if (creep.memory.twerking)
         {
+            var return_loc1 = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+            var return_loc3 = creep.room.controller;
             if (return_loc1 != null)
                 buildThis.run(creep, return_loc1);
             else
