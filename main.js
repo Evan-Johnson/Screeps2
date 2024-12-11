8f290dcb13bbf57554e8de41cff326833e41fa0a
Evan Johnson <johnsonevanpatrick@gmail.com>
2024-12-10 17:38:03 -0700

    Reducing CPU usage

--- main.js
+++ main.js
@@ -7,9 +7,11 @@ module.exports.loop = function ()
     //run creep manager
     creepyControl.run(Game.spawns.SPAWNY);
     
+    /*
     var tower_structure = Game.spawns.SPAWNY.room.find(FIND_STRUCTURES, {
        filter: (structure) => {
            return (structure.structureType == STRUCTURE_TOWER)
        } 
     });
+    */
 };
