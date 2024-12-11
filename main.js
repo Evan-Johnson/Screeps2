38d1c8737e611075707a590d8ee9062a65698fd8
Evan Johnson <johnsonevanpatrick@gmail.com>
2024-12-10 17:44:49 -0700

    Added screeps-profiler.js file to show CPU usage

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 var creepyControl = require('creepy_controller');
 var towerControl = require('tower_controller');
+const profiler = require('screeps-profiler');
 
-
+// This line monkey patches the global prototypes.
+profiler.enable();
+module.exports.loop = function() {
+  profiler.wrap(function() {
+    // Main.js logic should go here.
+    creepyControl.run(Game.spawns.SPAWNY);
+  });
+}
+/*
 module.exports.loop = function () 
 {
     //run creep manager
@@ -14,4 +23,4 @@ module.exports.loop = function ()
        } 
     });
     */
-};
+//};
