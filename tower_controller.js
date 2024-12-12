1a689dc90ccdf213dd176335d131d667130338cf
Evan Johnson <johnsonevanpatrick@gmail.com>
2024-12-11 23:52:48 -0700

    spacing in tower_controller. Trying to figure out how to optimize builders to not wait next to tower.

--- tower_controller.js
+++ tower_controller.js
@@ -8,16 +8,17 @@ module.exports = {
             tower.attack(hostiles[0]);
         } else 
         {
-        //create an array of all the structures that can be healed
-        var repairs = spawn.room.find(FIND_STRUCTURES, {
-            filter: object => (object.structureType != STRUCTURE_WALL) && (object.structureType != STRUCTURE_RAMPART) && object.hits < object.hitsMax // (object.structureType != STRUCTURE_WALL) &&
-        }); 
-        
-        //sort the array by immediate need
-        repairs.sort((a, b) => a.hits - b.hits);
-        
-        //repair those structures
-        tower.repair(repairs[0]);
+            //create an array of all the structures that can be healed
+            var repairs = spawn.room.find(FIND_STRUCTURES, {
+                filter: object => (object.structureType != STRUCTURE_WALL) && (object.structureType != STRUCTURE_RAMPART) && object.hits < object.hitsMax // (object.structureType != STRUCTURE_WALL) &&
+            }); 
+
+            //sort the array by immediate need
+            repairs.sort((a, b) => a.hits - b.hits);
+            
+            //repair those structures
+            tower.repair(repairs[0]);
+            
         }
     }
 };
