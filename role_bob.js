26629d1f7d274ec7da3efb7fcda9bc2820143d94
Evan Johnson <johnsonevanpatrick@gmail.com>
2024-12-12 00:04:55 -0700

    Builders now return to controller when there is nothing to build.

--- role_bob.js
+++ role_bob.js
@@ -5,20 +5,21 @@ var returnGold = require('return_gold');
 module.exports = {
     run: function (creep)
     {
-        
+        /*
         var return_loc2 = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                 filter: (s) => (s.structureType == STRUCTURE_TOWER) && s.energy < s.energyCapacity
         }, "astar");
-    
+        */
+
         //console.log("hey");
         if (creep.memory.twerking)
         {
             var return_loc1 = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-            //var return_loc3 = creep.room.controller;
+            var return_loc3 = creep.room.controller;
             if (return_loc1 != null)
                 buildThis.run(creep, return_loc1);
             else
-                returnGold.run(creep, return_loc2);
+                returnGold.run(creep, return_loc3);
         } else 
         {
             //console.log("hi");
