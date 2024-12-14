552da0efcef23fd8d0ddecd74d5c12718393efaf
Evan Johnson <johnsonevanpatrick@gmail.com>
2024-12-14 16:39:33 -0700

    cleaning functions

--- tower_controller.js
+++ tower_controller.js
@@ -2,21 +2,21 @@ module.exports = {
     run: function(tower, spawn)
     {
         var hostiles = spawn.room.find(FIND_HOSTILE_CREEPS);
-        //console.log(hostiles[0]);
+        // console.log(hostiles[0]);
         if (hostiles[0] != undefined)
         {
             tower.attack(hostiles[0]);
         } else 
         {
-            //create an array of all the structures that can be healed
+            // create an array of all the structures that can be healed
             var repairs = spawn.room.find(FIND_STRUCTURES, {
                 filter: object => (object.structureType != STRUCTURE_WALL) && (object.structureType != STRUCTURE_RAMPART) && object.hits < object.hitsMax // (object.structureType != STRUCTURE_WALL) &&
             }); 
 
-            //sort the array by immediate need
+            // sort the array by immediate need
             repairs.sort((a, b) => a.hits - b.hits);
             
-            //repair those structures
+            // repair those structures
             tower.repair(repairs[0]);
             
         }
