5b1189ce137c949adf76948749800ad14631b1a5
Evan Johnson <johnsonevanpatrick@gmail.com>
2025-01-05 21:42:01 -0700

    return voyager gold to the controller in the room.

--- role_voyager.js
+++ role_voyager.js
@@ -1,9 +1,12 @@
 var newRoom = require('new_room');
 var getGold = require('get_gold');
+var returnGold = require('return_gold');
 
 module.exports = {
     run: function(creep, room)
     {
+        var return_loc = creep.room.controller;
+
         if (creep.pos.roomName == room)
             creep.memory.rightRoom = true;
         else
@@ -12,7 +15,16 @@ module.exports = {
         if (!creep.memory.rightRoom && !creep.memory.twerking) //needs to go to new room
             newRoom.run(creep, room);
         else {
-            getGold.run(creep);
+            if (creep.memory.twerking) {
+                returnGold.run(creep, return_loc);
+
+                if (creep.carry.energy == 0)
+                    {
+                        creep.memory.twerking = false; 
+                    }
+            } else {
+                getGold.run(creep);
+            }
         }
 
     }};
